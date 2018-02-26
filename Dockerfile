9292cf4d408a4b9f9d77a77b57f98f9fb2ce2b99
administrator <jesus.dediego.erles@gmail.com>
2018-02-26 16:09:30 +0000

    Changed dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,40 +1,56 @@
-FROM openjdk:8-alpine
+FROM openjdk:8u151
 
-RUN apk --update add git curl tar bash ncurses && \
-    rm -rf /var/lib/apt/lists/* && \
-    rm /var/cache/apk/*
+# CONSTANTS
+ENV SCALA_VERSION 2.12.4
+ENV SBT_VERSION 1.1.1
+ENV SPARK_VERSION 2.2.1
 
-ARG SBT_VERSION=1.1.1
-ARG SBT_HOME=/usr/local/sbt
-RUN curl -sL "https://github.com/sbt/sbt/releases/download/v$SBT_VERSION/sbt-$SBT_VERSION.tgz" | tar -xz -C /usr/local
+RUN touch /usr/lib/jvm/java-8-openjdk-amd64/release
 
-ARG SPARK_VERSION=2.2.1
+RUN apt-get update && \
+    apt-get install -y net-tools
 
-ARG SPARK_HOME=/usr/local/spark-$SPARK_VERSION-bin-hadoop2.7
-RUN curl -sL "http://www-eu.apache.org/dist/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop2.7.tgz" | tar -xz -C /usr/local
+# 1-Install Scala
+RUN \
+  curl -fsL https://downloads.typesafe.com/scala/$SCALA_VERSION/scala-$SCALA_VERSION.tgz | tar xfz - -C /root/ && \
+  echo >> /root/.bashrc && \
+  echo "export PATH=~/scala-$SCALA_VERSION/bin:$PATH" >> /root/.bashrc
 
-RUN apk add --no-cache python3 && \ 
-    python3 -m ensurepip && \ 
-    rm -r /usr/lib/python*/ensurepip && \ 
-    pip3 install --upgrade pip setuptools && \ 
-    if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \ 
-    if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi && \ 
-    rm -r /root/.cache
 
-# Fix the value of PYTHONHASHSEED
-# Note: this is needed when you use Python 3.3 or greater
-ENV PYTHONHASHSEED 1
+# 2-Install sbt
+RUN \
+  curl -L -o sbt-$SBT_VERSION.deb https://dl.bintray.com/sbt/debian/sbt-$SBT_VERSION.deb && \
+  dpkg -i sbt-$SBT_VERSION.deb && \
+  rm sbt-$SBT_VERSION.deb && \
+  apt-get update && \
+  apt-get install sbt && \
+  sbt sbtVersion
+
+
+# 3-Install Spark
+ENV SPARK_PACKAGE spark-$SPARK_VERSION-bin-hadoop2.7
+ENV SPARK_HOME /usr/spark-${SPARK_VERSION}
+ENV SPARK_DIST_CLASSPATH="$HADOOP_HOME/etc/hadoop/*:$HADOOP_HOME/share/hadoop/common/lib/*:$HADOOP_HOME/share/hadoop/common/*:$HADOOP_HOME/share/hadoop/hdfs/*:$HADOOP_HOME/share/hadoop/hdfs/lib/*:$HADOOP_HOME/share/hadoop/hdfs/*:$HADOOP_HOME/share/hadoop/yarn/lib/*:$HADOOP_HOME/share/hadoop/yarn/*:$HADOOP_HOME/share/hadoop/mapreduce/lib/*:$HADOOP_HOME/share/hadoop/mapreduce/*:$HADOOP_HOME/share/hadoop/tools/lib/*"
+ENV PATH $PATH:${SPARK_HOME}/bin
+RUN curl --silent \
+         --show-error \
+         --location \
+         --retry 3 \
+        "http://www-eu.apache.org/dist/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop2.7.tgz" | \
+    tar -xz -C /usr/ && \
+    mv /usr/$SPARK_PACKAGE $SPARK_HOME && \
+chmod -R 7777 $SPARK_HOME
 
 ENV PATH $PATH:$SBT_HOME/bin:$SPARK_HOME/bin
-
 ENV SPARK_MASTER local[*]
-
 ENV SPARK_DRIVER_PORT 5001
 ENV SPARK_UI_PORT 5002
 ENV SPARK_BLOCKMGR_PORT 5003
 EXPOSE $SPARK_DRIVER_PORT $SPARK_UI_PORT $SPARK_BLOCKMGR_PORT
 
+WORKDIR /root
 
 COPY run.sh /
 RUN chmod +x /run.sh
-CMD ./run.sh
+CMD /run.sh
+
--- run.sh
+++ run.sh
@@ -6,24 +6,24 @@ set -o errexit
 test -z "$DRIVER_APP_TYPE" && ( echo "DRIVER_APP_TYPE is not set; exiting" ; exit 1 )
 test -z "$SCM_URL" && ( echo "SCM_URL is not set; exiting" ; exit 1 )
 test -z "$SPARK_MASTER_URL" && ( echo "SPARK_MASTER_URL is not set; exiting" ; exit 1 )
-test -z "$SPARK_DRIVER_HOST" && ( echo "SPARK_DRIVER_HOST is not set; exiting" ; exit 1 )
-test -z "$SPARK_DRIVER_PORT" && ( echo "SPARK_DRIVER_PORT is not set; exiting" ; exit 1 )
-test -z "$SPARK_UI_PORT" && ( echo "SPARK_UI_PORT is not set; exiting" ; exit 1 )
 test -z "$SPARK_BLOCKMGR_PORT" && ( echo "SPARK_BLOCKMGR_PORT is not set; exiting" ; exit 1 )
 
-
+echo "STEP: Get the container internal IP (spark.driver.bindAddress)"
 ip_addr="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]+\.){3}[0-9]+' | grep -Eo '([0-9]+\.){3}[0-9]+' | grep -v '127.0.0.1' | head)"
 if test -z "$ip_addr"
 then
   echo "Cannot determine the container IP address."
   exit 1
 else
-  echo "The container IP address is $ip_addr"
+  echo "The container IP address is $ip_addr. It will be set as spark.driver.bindAddress"
 fi
+
+SPARK_DRIVER_PORT="7077"
+SPARK_UI_PORT="4040"
 
 if [ "$DRIVER_APP_TYPE" = "scala" ] || [ "$DRIVER_APP_TYPE" = "python" ]
 then
-  echo "Cloning the repository: $SCM_URL"
+  echo "STEP: Cloning the repository of the driver application: $SCM_URL"
   git clone "$SCM_URL"
   project_git="${SCM_URL##*/}"
   project_dir="${project_git%.git}"
@@ -36,21 +36,24 @@ then
 
 elif [ "$DRIVER_APP_TYPE" = "jar" ]
 then
-  echo "Downloading JAR file $REMOTE_JAR_FILE"
+  echo "---------- MODE: JAR ----------"
+  echo "STEP: Downloading JAR file of the driver application: $REMOTE_JAR_FILE"
   
   remoteJarFile=$(basename "$REMOTE_JAR_FILE")
   wget $REMOTE_JAR_FILE
 
+  echo "----------Driver application information----------"
   echo "Handling JAR file $remoteJarFile"
   echo "Application main class: $MAIN_CLASS"
   echo "Application arguments: $APP_ARGS"
   echo "Spark master: $SPARK_MASTER_URL"
+  echo "Spark bind address: $ip_addr"
   echo "Spark driver host: $SPARK_DRIVER_HOST"
   echo "Spark driver port: $SPARK_DRIVER_PORT"
   echo "Spark UI port: $SPARK_UI_PORT"
   echo "Spark block manager port: $SPARK_BLOCKMGR_PORT"
   echo "Spark configuration settings: $SPARK_CONF"
-
+ echo "---------------------------------------------------"
 
   spark-submit \
     --master "$SPARK_MASTER_URL" \
@@ -67,15 +70,15 @@ fi
 
 if [ "$DRIVER_APP_TYPE" = "scala" ]
 then
-
-  echo "Building the jar"
+  echo "---------- MODE: Scala ----------"
+  echo "STEP: Start building the jar"
   if test -z "$PROJECT_SUBDIR"
   then
     cd "$project_dir"
   else
     cd "$project_dir/$PROJECT_SUBDIR"
   fi
-  echo "Building at: $(pwd)"
+  echo "STEP: Building at: $(pwd)"
 
   test -z "$MAIN_CLASS" && ( echo "MAIN_CLASS is not set; exiting" ; exit 1 )
   if test -z "$BUILD_COMMAND"
@@ -84,7 +87,7 @@ then
   else
     build_command="$BUILD_COMMAND"
   fi
-  echo "Building with command: $build_command"
+  echo "STEP: Building with command: $build_command"
   eval $build_command
 
   if test -z "$JAR_FILE"
@@ -95,10 +98,12 @@ then
   fi
   if ! test -f "$jarfile"
   then
-    echo "Jar file not found or not a single file: $jarfile"
+    echo "ERROR: Jar file not found or not a single file: $jarfile"
     echo "You can specify jar file explicitly: -e JAR_FILE=/path/to/file"
     exit 1
   fi
+
+  echo "----------Driver application information----------"
   echo "Handling Scala application"
   echo "Submitting JAR file: $jarfile"
   echo "Application main class: $MAIN_CLASS"
@@ -109,6 +114,8 @@ then
   echo "Spark UI port: $SPARK_UI_PORT"
   echo "Spark block manager port: $SPARK_BLOCKMGR_PORT"
   echo "Spark configuration settings: $SPARK_CONF"
+ echo "---------------------------------------------------"
+
 
   spark-submit \
     --master "$SPARK_MASTER_URL" \
@@ -124,6 +131,7 @@ then
 
 elif [ "$DRIVER_APP_TYPE" = "python" ]
 then
+  echo "---------- MODE: Python ----------"
   if test -z "$PYTHON_FILE"
   then
     pythonfile="$project_dir/main.py"
@@ -132,11 +140,12 @@ then
   fi
   if ! test -f "$pythonfile"
   then
-    echo "Python file not found or not a single file: $pythonfile"
+    echo "ERROR: Python file not found or not a single file: $pythonfile"
     echo "You can specify a python file explicitly: -e PYTHON_FILE=/path/to/file"
     exit 1
   fi
 
+  echo "----------Driver application information----------"
   echo "Submitting python script: $pythonfile"
   echo "Application arguments: $APP_ARGS"
   echo "Spark master: $SPARK_MASTER_URL"
@@ -145,6 +154,7 @@ then
   echo "Spark UI port: $SPARK_UI_PORT"
   echo "Spark block manager port: $SPARK_BLOCKMGR_PORT"
   echo "Spark configuration settings: $SPARK_CONF"
+  echo "---------------------------------------------------"
 
 spark-submit \
     --master "$SPARK_MASTER_URL" \
@@ -157,7 +167,7 @@ spark-submit \
     $PYTHON_FILE \ 
     $APP_ARGS
 else
-  echo "Not recognized type of application: scala|python."
+  echo "ERROR: Not recognized type of application (DRIVER_APP_TYPE): scala|jar|python."
 fi
 
 
